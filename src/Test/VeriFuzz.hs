178fc86634cd6d10fee44434de215509974d9f93
Yann Herklotz <ymherklotz@gmail.com>
2018-11-09 20:33:09 +0000

    Add main module

---
+++ src/Test/VeriFuzz.hs
@@ -0,0 +1,13 @@
+module Test.VeriFuzz
+  (
+    -- * Definitions
+    module Test.VeriFuzz.Types
+    -- * Code Generation
+  , module Test.VeriFuzz.CodeGen
+    -- * Graphs
+  , module Test.VeriFuzz.Graph.Random
+  ) where
+
+import Test.VeriFuzz.Types
+import Test.VeriFuzz.Graph.Random
+import Test.VeriFuzz.CodeGen
