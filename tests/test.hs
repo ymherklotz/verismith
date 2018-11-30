8b22145bf32c96067358193388a54621d3826628
Yann Herklotz <ymherklotz@gmail.com>
2018-11-30 21:57:59 +0000

    Restructure and add tests

---
+++ tests/test.hs
@@ -0,0 +1,17 @@
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck as QC
+
+unitTests = testGroup "Unit tests"
+  [ testCase "List comparison (different length)" $
+      [1, 2, 3] `compare` [1,2] @?= GT
+
+  -- the following test does not hold
+  , testCase "List comparison (same length)" $
+      [1, 2, 3] `compare` [1,2,2] @?= LT
+  ]
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+main = defaultMain tests
