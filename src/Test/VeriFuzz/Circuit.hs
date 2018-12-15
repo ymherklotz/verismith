86bc5c8596085d308df5d2c6ae5522bfb0ba50e0
Yann Herklotz <ymherklotz@gmail.com>
2018-12-15 20:17:54 +0000

    Rename types to circuit

--- src/Test/VeriFuzz/Circuit.hs
+++ src/Test/VeriFuzz/Circuit.hs
@@ -1,14 +1,28 @@
-module Test.VeriFuzz.Types where
+{-|
+Module      : Test.VeriFuzz.Circuit
+Description : Definition of the circuit graph.
+Copyright   : (c) Yann Herklotz Grave 2018
+License     : GPL-3
+Maintainer  : ymherklotz@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+Definition of the circuit graph.
+-}
+
+module Test.VeriFuzz.Circuit where
 
 import           Data.Graph.Inductive
 import           System.Random
 import           Test.QuickCheck
 
+-- | The types for all the gates.
 data Gate = And
           | Or
           | Xor
           deriving (Show, Eq, Enum, Bounded, Ord)
 
+-- | Newtype for the Circuit which implements a Graph from fgl.
 newtype Circuit = Circuit { getCircuit :: Gr Gate () }
 
 instance Random Gate where
