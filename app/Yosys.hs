0bbae2481444d02413df374ddde1a9bd8342b24a
Yann Herklotz <ymherklotz@gmail.com>
2018-12-27 14:40:47 +0100

    Add yosys shell script

---
+++ app/Yosys.hs
@@ -0,0 +1,18 @@
+{-|
+Module      : Main
+Description : Main Yosys runner
+Copyright   : (c) Yann Herklotz Grave 2018
+License     : GPL-3
+Maintainer  : ymherklotz@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+Main Yosys runner
+-}
+
+module Main where
+
+import           Shelly
+
+main :: IO ()
+main = putStrLn "Hello"
