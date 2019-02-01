1067284cc1f6ca8ba646545c5b8d0a79cc2e41ad
Yann Herklotz <ymherklotz@gmail.com>
2019-02-01 19:39:52 +0000

    More restructuring

---
+++ src/VeriFuzz/Env.hs
@@ -0,0 +1,26 @@
+{-|
+Module      : VeriFuzz.Env
+Description : Environment to run the simulator and synthesisers in a matrix.
+Copyright   : (c) 2019, Yann Herklotz Grave
+License     : GPL-3
+Maintainer  : ymherklotz [at] gmail [dot] com
+Stability   : experimental
+Portability : POSIX
+
+Environment to run the simulator and synthesisers in a matrix.
+-}
+
+module VeriFuzz.Env where
+
+-- | Environment used to run the main
+data SimMatrix = SimMatrix { yosys  :: Yosys
+                           , xst    :: Maybe Xst
+                           , icarus :: Maybe Icarus
+                           }
+
+type SimEnv = ReaderT SimMatrix IO
+
+runAll :: SimEnv ()
+runAll = do
+  val <- asks xst
+  shelly $ run_ "echo" ["Hello World"]
