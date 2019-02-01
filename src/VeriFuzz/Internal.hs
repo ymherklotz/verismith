a9002d9bfdfa8b6ae0973b7972ce300142c1ded0
Yann Herklotz <ymherklotz@gmail.com>
2019-02-01 19:43:22 +0000

    Fix internal modules

--- src/VeriFuzz/Internal.hs
+++ src/VeriFuzz/Internal.hs
@@ -10,7 +10,17 @@ Portability : POSIX
 Shared high level code used in the other modules internally.
 -}
 
-module VeriFuzz.Internal where
+module VeriFuzz.Internal
+  ( -- * Useful functions
+    safe
+    -- * Circuit modules
+  , module VeriFuzz.Internal.Circuit
+    -- * Simulator Internals
+  , module VeriFuzz.Internal.Simulator
+  ) where
+
+import           VeriFuzz.Internal.Circuit
+import           VeriFuzz.Internal.Simulator
 
 -- | Converts unsafe list functions in the Prelude to a safe version.
 safe :: ([a] -> b) -> [a] -> Maybe b
