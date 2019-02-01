e6976ea66dad180203e1e0f678ac56c515a6878a
Yann Herklotz <ymherklotz@gmail.com>
2019-02-01 11:38:19 +0000

    Make code a bit more readable

--- src/VeriFuzz/Verilog/Mutate.hs
+++ src/VeriFuzz/Verilog/Mutate.hs
@@ -159,8 +159,9 @@ makeTop i m = ModDecl (m ^. modId) ys (m ^. modInPorts) modIt
   modN n = m & modId %~ makeIdFrom n & modOutPorts .~ [Port Wire 90 (makeIdFrom n "y")]
 
 makeTopAssert :: ModDecl -> ModDecl
-makeTopAssert = (modItems %~ (++ [assert])) . (modInPorts %~ ((Port Wire 1 "clk") :)) . makeTop 2
+makeTopAssert = (modItems %~ (++ [assert])) . (modInPorts %~ addClk) . makeTop 2
  where
   assert = Always . EventCtrl e . Just $ SeqBlock
     [TaskEnable $ Task "assert" [BinOp (Id "y_1") BinEq (Id "y_2")]]
   e = EPosEdge "clk"
+  addClk = ((Port Wire 1 "clk") :)
