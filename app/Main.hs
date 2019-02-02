c03e629452312105c3a2db638a478fe0acd63ac9
Yann Herklotz <ymherklotz@gmail.com>
2019-02-02 13:43:20 +0000

    Smaller test case

--- app/Main.hs
+++ app/Main.hs
@@ -78,5 +78,5 @@ main :: IO ()
 main = do
   num <- getNumCapabilities
   vars <- sequence $ (\x -> myForkIO $
-                       runEquivalence (randomMod 10 100) ("test_" <> T.pack (show x)) 0) <$> [1..num]
+                       runEquivalence (randomMod 5 50) ("test_" <> T.pack (show x)) 0) <$> [1..num]
   sequence_ $ takeMVar <$> vars
