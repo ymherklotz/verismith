eabceca934a88c64940e04d10569585909b0770b
Yann Herklotz Grave <git@yannherklotzgrave.com>
2019-03-16 10:41:31 +0000

    Add scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo yum -y update
+sudo yum -y install git gcc gcc-c++ tcl-devel python3 \
+     graphviz xdot gperf gmp-devel make bison flex autoconf \
+     tmux
+
+sudo mkdir -p /mnt/tools
+sudo mount /dev/sdf /mnt/tools
+
+sudo chown -R $USER:$USER /mnt/tools/home/ec2-user
+
+curl -sSL https://get.haskellstack.org/ | sh
+
+{ cat <<EOF
+export PATH="\${PATH}:/mnt/tools/bin:/mnt/tools/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/"
+EOF
+} >> $HOME/.bashrc
+
+source $HOME/.bashrc
