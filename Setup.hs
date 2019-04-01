ce3b5a9dc47c2325e1e9cc61279972048b9fbabd
Yann Herklotz <git@ymhg.org>
2019-04-01 10:52:06 +0100

    Add back fixed doctest

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,4 @@
-import           Distribution.Simple
-main = defaultMain
+import           Distribution.Extra.Doctest
+
+main :: IO ()
+main = defaultMainWithDoctests "verifuzz-doctest"
--- test/Doctest.hs
+++ test/Doctest.hs
@@ -1,1 +1,12 @@
-{-# options_ghc -F -pgmF doctest-discover -optF test/doctest.json #-}
+module Main where
+
+import           Build_doctests (flags, module_sources, pkgs)
+import           Data.Foldable  (traverse_)
+import           Test.DocTest   (doctest)
+
+main :: IO ()
+main = do
+    traverse_ putStrLn args -- optionally print arguments
+    doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
