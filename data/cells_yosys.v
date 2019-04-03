6776d38b11186e97101995eb2c071096cc1d648b
Yann Herklotz <git@ymhg.org>
2019-04-03 19:52:15 +0100

    Add missing modules when using always blocks

---
+++ data/cells_yosys.v
@@ -0,0 +1,13 @@
+// Taken from yosys verilog files.
+
+module \$_DLATCH_N_ (E, D, Q);
+   wire [1023:0] _TECHMAP_DO_ = "simplemap; opt";
+   input         E, D;
+   output        Q = !E ? D : Q;
+endmodule
+
+module \$_DLATCH_P_ (E, D, Q);
+   wire [1023:0] _TECHMAP_DO_ = "simplemap; opt";
+   input         E, D;
+   output        Q = E ? D : Q;
+endmodule
