e22a59ad643ac2fe96b0c11208651a6f7a6605b0
Yann Herklotz <git@ymhg.org>
2019-04-12 17:15:39 +0100

    Fix the setup script for aws

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 sudo yum -y update
-sudo yum -y install git gcc gcc-c++ tcl-devel python3 \
+sudo yum -y install git gcc gcc-c++ tcl-devel python36 \
      graphviz xdot gperf gmp-devel make bison flex autoconf \
      tmux
 
 sudo mkdir -p /mnt/tools
-sudo mount /dev/sdf1 /mnt/tools
+sudo mount /dev/nvme2n1p1 /mnt/tools
 
 sudo chown -R $USER:$USER /mnt/tools/home/ec2-user
 
 curl -sSL https://get.haskellstack.org/ | sh
 
 { cat <<EOF
-export PATH="\${PATH}:/mnt/tools/bin:/mnt/tools/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/"
+export PATH="\${PATH}:/mnt/tools/bin:/mnt/tools/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/:/mnt/tools/opt/Xilinx/Vivado/2018.3/bin"
 EOF
 } >> $HOME/.bashrc
 
