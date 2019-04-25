1486a2afa481de46938c1bc122c469975978593f
Yann Herklotz <git@ymhg.org>
2019-04-25 16:17:03 +0100

    Add --num command line option

--- app/Main.hs
+++ app/Main.hs
@@ -21,6 +21,7 @@ data Opts = Fuzz { fuzzOutput :: {-# UNPACK #-} !Text
                  , configFile :: !(Maybe FilePath)
                  , forced     :: !Bool
                  , keepAll    :: !Bool
+                 , num        :: {-# UNPACK #-} !Int
                  }
           | Generate { mFileName  :: !(Maybe FilePath)
                      , configFile :: !(Maybe FilePath)
@@ -78,6 +79,14 @@ fuzzOpts =
             )
         <*> (switch $ long "keep" <> short 'k' <> help
                 "Keep all the directories."
+            )
+        <*> (option auto $
+             long "num"
+             <> short 'n'
+             <> help "The number of fuzz runs that should be performed."
+             <> showDefault
+             <> value 1
+             <> metavar "INT"
             )
 
 genOpts :: Parser Opts
@@ -197,11 +206,11 @@ getConfig :: Maybe FilePath -> IO V.Config
 getConfig = maybe (return V.defaultConfig) V.parseConfigFile
 
 handleOpts :: Opts -> IO ()
-handleOpts (Fuzz out configF _ _) = do
+handleOpts (Fuzz out configF _ _ n) = do
     config <- getConfig configF
     _ <- V.runFuzz config
                    V.defaultYosys
-                   (V.fuzzMultiple 5 Nothing (V.proceduralSrc "top" config))
+                   (V.fuzzMultiple n Nothing (V.proceduralSrc "top" config))
     return ()
 handleOpts (Generate f c) = do
     config <- getConfig c
