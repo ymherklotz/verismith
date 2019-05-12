1b35ee6626faeef57a049177ace3bdc7f2111ca7
Yann Herklotz <git@ymhg.org>
2019-05-12 16:46:50 +0100

    Fix setup script for new aws

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,16 +2,23 @@
 
 sudo yum -y update
 
-# In addition, need to install bison-3.3
-sudo yum -y install git gcc72 gcc72-c++ tcl-devel python36 \
+sudo yum -y install git gcc gcc-c++ tcl-devel python3 \
      graphviz xdot gperf gmp-devel make flex autoconf \
-     readline-devel clang6.0 tmux boost libffi-devel
+     readline-devel tmux boost libffi-devel bison ncurses-compat-libs
 
 sudo mkdir -p /mnt/tools
+sudo mkdir -p /mnt/work
+
+sudo mount /dev/nvme2n1p1 /mnt/tools
+sudo mount /dev/nvme1n1p1 /mnt/work
+
+sudo chown -R ec2-user:ec2-user /mnt/tools/home/ec2-user
+sudo chown -R ec2-user:ec2-user /mnt/work
 
 curl -sSL https://get.haskellstack.org/ | sh
 
 { cat <<EOF
+export PATH="/mnt/tools/opt/yosys/master/bin:\${PATH}"
 export PATH="\${PATH}:/mnt/tools/bin"
 export PATH="\${PATH}:/mnt/tools/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64"
 export PATH="\${PATH}:/mnt/tools/opt/Xilinx/Vivado/2018.3/bin"
