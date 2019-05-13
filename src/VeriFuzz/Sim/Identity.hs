b5fddf170e4f4d798b0411f417735cec21e20b29
Yann Herklotz <git@ymhg.org>
2019-05-13 14:57:37 +0100

    Add Identity synthesiser

---
+++ src/VeriFuzz/Sim/Identity.hs
@@ -0,0 +1,44 @@
+{-|
+Module      : VeriFuzz.Sim.Identity
+Description : The identity simulator and synthesiser.
+Copyright   : (c) 2019, Yann Herklotz Grave
+License     : GPL-3
+Maintainer  : ymherklotz [at] gmail [dot] com
+Stability   : experimental
+Portability : POSIX
+
+The identity simulator and synthesiser.
+-}
+
+module VeriFuzz.Sim.Identity where
+
+import           Control.DeepSeq          (NFData, rnf, rwhnf)
+import           Data.Text                (Text, unpack)
+import           Prelude                  hiding (FilePath)
+import           Shelly                   (FilePath)
+import           Shelly.Lifted            (writefile)
+import           VeriFuzz.Sim.Internal
+import           VeriFuzz.Verilog.AST
+import           VeriFuzz.Verilog.CodeGen
+
+data Identity = Identity { identityDesc   :: {-# UNPACK #-} !Text
+                         , identityOutput :: {-# UNPACK #-} !FilePath
+                         }
+
+instance Tool Identity where
+    toText (Identity d _) = d
+
+instance Show Identity where
+    show t = unpack $ toText t
+
+instance Synthesiser Identity where
+    runSynth = runSynthIdentity
+    synthOutput = identityOutput
+    setSynthOutput (Identity a _) = Identity a
+
+instance NFData Identity where
+    rnf = rwhnf
+
+runSynthIdentity :: Identity -> SourceInfo -> ResultSh ()
+runSynthIdentity (Identity _ out) src =
+    writefile out $ genSource src
