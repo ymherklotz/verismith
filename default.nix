d57842487d86d710bdbf83b9c2bbe1fa8c1dd6e2
Yann Herklotz <git@yannherklotz.com>
2019-07-27 11:16:17 +0200

    Make the nix build more concise

--- default.nix
+++ default.nix
@@ -1,36 +1,12 @@
-{ mkDerivation, alex, array, base, binary, blaze-html, bytestring
-, Cabal, cabal-doctest, criterion, cryptonite, deepseq, DRBG
-, exceptions, fgl, fgl-visualize, filepath, gitrev, hedgehog
-, hedgehog-fn, lens, lifted-base, memory, monad-control
-, optparse-applicative, parsec, prettyprinter, random
-, recursion-schemes, shakespeare, shelly, statistics, stdenv, tasty
-, tasty-hedgehog, tasty-hunit, template-haskell, text, time
-, tomland, transformers, transformers-base, unordered-containers
-, vector
-}:
-mkDerivation {
-  pname = "verifuzz";
-  version = "0.3.1.0";
-  src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  setupHaskellDepends = [ base Cabal cabal-doctest ];
-  libraryHaskellDepends = [
-    array base binary blaze-html bytestring cryptonite deepseq DRBG
-    exceptions fgl fgl-visualize filepath gitrev hedgehog lens
-    lifted-base memory monad-control optparse-applicative parsec
-    prettyprinter random recursion-schemes shakespeare shelly
-    statistics template-haskell text time tomland transformers
-    transformers-base unordered-containers vector
-  ];
-  libraryToolDepends = [ alex ];
-  executableHaskellDepends = [ base ];
-  testHaskellDepends = [
-    base fgl hedgehog hedgehog-fn lens parsec shakespeare tasty
-    tasty-hedgehog tasty-hunit text
-  ];
-  benchmarkHaskellDepends = [ base criterion lens ];
-  homepage = "https://github.com/ymherklotz/VeriFuzz#readme";
-  description = "Random verilog generation and simulator testing";
-  license = stdenv.lib.licenses.bsd3;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865", doBenchmark ? false } :
+let
+  haskellPackages = nixpkgs.pkgs.haskellPackages.override {
+    overrides = haskellPackagesNew: haskellPackagesOld: rec {
+        hedgehog-fn = haskellPackagesNew.callPackage ./nix/hedgehog-fn.nix {};
+        tomland = nixpkgs.pkgs.haskell.lib.dontCheck (haskellPackagesNew.callPackage ./nix/tomland.nix {});
+        parser-combinators = haskellPackagesNew.callPackage ./nix/parser-combinators.nix {};
+        tasty-hedgehog = haskellPackagesNew.callPackage ./nix/tasty-hedgehog.nix {};
+      };
+  };
+in
+  haskellPackages.callCabal2nix "verifuzz" (./.) {}
