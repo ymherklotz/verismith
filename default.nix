41c88cd7cad64761a6fe783c5225a1de187c5d87
Yann Herklotz <git@yannherklotz.com>
2019-10-06 23:19:19 +0100

    Pin the nix repository

--- default.nix
+++ default.nix
@@ -1,6 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865", doBenchmark ? false } :
+{ nixpkgs ? null, compiler ? "ghc865", doBenchmark ? false } :
 let
-  variant = if doBenchmark then nixpkgs.pkgs.haskell.lib.doBenchmark else nixpkgs.pkgs.lib.id;
-  verismith = nixpkgs.pkgs.haskellPackages.callCabal2nix "verismith" (./.) {};
+  sysPkg = import <nixpkgs> { };
+  pinnedPkg = builtins.fetchGit {
+    name = "nixos-unstable-2019-10-06";
+    url = https://github.com/nixos/nixpkgs/;
+    rev = "271fef8a4eb03cd9de0c1fe2f0b7f4a16c2de49a";
+  };
+  npkgs = if nixpkgs == null then
+           import pinnedPkg {}
+         else
+           import nixpkgs {};
+  variant = if doBenchmark then npkgs.pkgs.haskell.lib.doBenchmark else npkgs.pkgs.lib.id;
+  verismith = npkgs.pkgs.haskellPackages.callCabal2nix "verismith" (./.) {};
 in
 variant verismith
