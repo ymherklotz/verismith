817f1c7ba3b2bd43b2ed7164b90008871850b913
Yann Herklotz <git@yannherklotz.com>
2020-03-07 14:25:04 +0000

    Add not condition

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-if [[ -z $NIX ]]; then
-  if [[ $NIX -eq 0 ]]; then
+if ! [[ -z $NIX ]]; then
+    if [[ $NIX -eq 0 ]]; then
     cabal update
     cabal build
   else
     nix-build
   fi
 fi
-
